3776a50e776583085de6bed45bbedf6b62f9decd
ParvesPR <parves2585@gmail.com>
2024-08-03 19:12:39 +0800

    how to work creation & execution phase

---
+++ variableAssign.js
@@ -0,0 +1,19 @@
+var a = 10;
+var b;
+console.log(a);
+console.log(b); /* declare before assign value */
+b = 20; /* assign value */
+console.log(b);
+console.log(c); /* declare before assign value */
+var c = 30;
+console.log(c);
+
+// Creation phase
+// a= undefined
+// b= undefined
+// c= undefined
+
+// Execution phase
+// a=10
+// b= undefined
+// c= undefined
