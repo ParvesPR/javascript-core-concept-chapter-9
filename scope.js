545b5a6931732bdb731e7f1b135149694f7d334a
ParvesPR <parves2585@gmail.com>
2024-08-17 12:59:20 +0800

    example of scope

---
+++ scope.js
@@ -0,0 +1,7 @@
+// Scope determines the accessibility of variables, objects, and functions from different parts of the code.
+var x = 55;
+function simple() {
+    x = 45;
+    console.log(x);
+}
+simple()
