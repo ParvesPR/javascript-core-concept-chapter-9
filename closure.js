49d270dd9d2f5605ab244ad2a1e8654925b562e0
ParvesPR <parves2585@gmail.com>
2024-08-23 12:57:08 +0800

    using loop and closure together

--- closure.js
+++ closure.js
@@ -1,8 +1,20 @@
+// Closure is the data used in a function, which data is not declared in the scope of this function, that is, in this own function, this data is not owned. This data is coming from outside, means it is coming from parent function.
 function init() {
-    var msg= 'I am Learning Closure'
+    var msg = 'I am Learning Closure'
     return function () {
         console.log(msg);
     }
 }
-var print= init(); /* Creates the closure */
+var print = init(); /* Creates the closure */
 print()
+
+// use loop and closure
+for (var x = 1; x <= 5; x++) {
+    (function (n) {
+
+        setTimeout(function () {
+            console.log(n);
+        }, 1000 * n);
+    }(x)
+    )
+}
